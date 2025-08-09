0f055117953f2296056b14397aa4206634bb57af
andrei <andrei@clinicsoftware.com>
2025-08-09 19:17:30 +0100

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { addCart } from '../redux/action';
 
